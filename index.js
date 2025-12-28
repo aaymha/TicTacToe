e3cb3c268c541eb621e855caae151fde66860944
Aaymha <szymon7710@icloud.com>
2025-12-28 17:03:22 +0100

    Wtf

--- index.js
+++ index.js
@@ -1,95 +1,101 @@
 function CreatePlayer(name, mark) {
-    const moves = [];
-
-    return {name, mark, moves: []};
+  return {name, mark, moves: []};
 }
+
 const Player1 = CreatePlayer("Simon", "X");
 const Player2 = CreatePlayer("Paj", "O");
 
 const DisplayDom = (() => {
-    const boxArea = document.querySelectorAll(".box");
+  const boxArea = document.querySelectorAll(".box");
 
-    function populateBoard() {
-        boxArea.forEach(box => {
-            box.addEventListener("click", function (e) {
-                GameController.makeMove(parseInt(e.target.id));
-            });
-        });
-    }
+  function populateBoard() {
+    boxArea.forEach(box => {
+      box.addEventListener("click", function (e) {
+        GameController.makeMove(parseInt(e.target.id));
+      });
+    });
+  }
 
-    function updateBox(boxId, mark) {
-        boxArea[boxId].textContent = mark;
-    }
+  function updateBox(boxId, mark) {
+    boxArea[boxId].textContent = mark;
+  }
 
-    function isBoxEmpty(boxId) {
-        return boxArea[boxId].textContent === "";
-    }
+  function isBoxEmpty(boxId) {
+    return boxArea[boxId].textContent === "";
+  }
 
-    function clearBoard() {
-        document.querySelectorAll(".box").forEach(box => {
-            box.textContent = "";
-        });
-    }
+  function clearBoard() {
+    document.querySelectorAll(".box").forEach(box => {
+      box.textContent = "";
+    });
+  }
 
-    return {populateBoard, updateBox, isBoxEmpty, clearBoard};
+  return {populateBoard, updateBox, isBoxEmpty, clearBoard};
 })();
 
 const GameController = (() => {
-    let currentPlayer = Player1;
+  let currentPlayer = Player1;
 
-    const switchPlayer = () => {
-        currentPlayer = currentPlayer === Player1 ? Player2 : Player1;
-    };
+  const switchPlayer = () => {
+    currentPlayer = currentPlayer === Player1 ? Player2 : Player1;
+  };
 
-    const checkForWinner = () => {
-        const winningCombinations = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-        return winningCombinations.some(combo =>
-            combo.every(num => currentPlayer.moves.includes(num))
-        );
+  const checkForWinner = () => {
+    const winningCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    return winningCombinations.some(combo =>
+      combo.every(num => currentPlayer.moves.includes(num))
+    );
+  }
+
+  const checkForTie = () => {
+    return Player1.moves.length + Player2.moves.length >= 8
+  }
+
+  const makeMove = (boxId) => {
+    if (!DisplayDom.isBoxEmpty(boxId)) return;
+
+    currentPlayer.moves.push(boxId);
+    DisplayDom.updateBox(boxId, currentPlayer.mark);
+
+    if (checkForWinner()) {
+      alert(`${currentPlayer.name} has won!`)
+      startGame();
     }
 
-    const checkForTie = () => {
-        return Player1.moves.length + Player2.moves.length >= 8
+    if (checkForTie()) {
+      alert("Its a tie!");
+      startGame();
     }
 
-   const makeMove = (boxId) => {
-        if(!DisplayDom.isBoxEmpty(boxId)) return;
+    switchPlayer();
+  }
 
-        currentPlayer.moves.push(boxId);
-        DisplayDom.updateBox(boxId, currentPlayer.mark);
+  const resetGame = () => {
+    document.getElementById("reset-board").addEventListener("click", () => {
+      DisplayDom.clearBoard();
+      startGame();
+    });
+  }
 
-        if(checkForWinner()) {
-            alert(`${currentPlayer.name} has won!`)
-            startGame();
-        }
+  const startGame = () => {
+    currentPlayer = Player1;
+    Player1.moves = [];
+    Player2.moves = [];
+    DisplayDom.clearBoard();
+    DisplayDom.populateBoard();
+    resetGame();
+  };
 
-        if(checkForTie()) {
-            alert("Its a tie!");
-            startGame();
-        }
-
-        switchPlayer();
-   }
-
-    const startGame = () => {
-        currentPlayer = Player1;
-        Player1.moves = [];
-        Player2.moves = [];
-        DisplayDom.clearBoard();
-        DisplayDom.populateBoard();
-
-    };
-
-    return {startGame, makeMove};
+  return {startGame, makeMove};
 })();
 
 GameController.startGame();
